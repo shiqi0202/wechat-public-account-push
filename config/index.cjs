f637a7bcf4a7434779636cd5efda001f220c4504
shiqi0202 <114892076+shiqi0202@users.noreply.github.com>
2022-10-03 11:12:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	AXHjckezf9dI3P2ogRonWizx50WbssRcjWOxp4MlCTU	',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGi-w6je91a3L40tAqHWMaMMCC3w',
+      id: '',
     }
   ],
 
